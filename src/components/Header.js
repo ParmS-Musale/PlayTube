53412d4299cde7ac45e8cfdd069dcc606ce4627d
ParmS-Musale <musaleparm9541@gmail.com>
2024-09-30 20:16:10 +0530

    All Set..!!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SlMenu } from "react-icons/sl";
 import { FaUserCircle } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
 import { useDispatch } from "react-redux";
-import {toggleMenu } from "../utils/appSlice";
-
+import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constant";
 
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchQuery) {
+        getSearchSuggestions();
+      }
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+  };
+
   const dispatch = useDispatch();
 
   const toggleMenuHandler = () => {
-    dispatch(toggleMenu())
-  }
+    dispatch(toggleMenu());
+  };
+
   return (
     <div className="flex items-center justify-between p-4 bg-white shadow-md">
       {/* Left Section - Logo & Menu */}
       <div className="flex items-center space-x-4">
-        <SlMenu className="text-2xl cursor-pointer" onClick={()=> toggleMenuHandler()}/>
-        <img 
+        <SlMenu
+          className="text-2xl cursor-pointer"
+          onClick={() => toggleMenuHandler()}
+        />
+        <img
           src="https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6.jpg"
           alt="yt-logo"
           className="h-12 cursor-pointer text-2xl"
@@ -25,15 +50,34 @@ const Header = () => {
       </div>
 
       {/* Middle Section - Search Bar */}
-      <div className="flex items-center w-1/2">
+      <div className="relative flex items-center w-1/2">
         <input
+          className="px-5 w-full border border-gray-400 p-2 rounded-l-full"
           type="text"
-          className="w-full p-2 border border-gray-300 rounded-l-full focus:outline-none focus:ring-2 focus:ring-blue-300"
-          placeholder="Search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={() => setShowSuggestions(true)}
+          onBlur={() => setShowSuggestions(false)}
         />
-        <button className="p-2 bg-gray-100 border border-gray-300 rounded-r-full hover:bg-gray-200">
-          <FaSearch className="text-2xl"/>
+        <button className="p-3 bg-gray-100 border border-gray-300 rounded-r-full hover:bg-gray-200">
+          <FaSearch />
         </button>
+
+        {/* Suggestions Dropdown */}
+        {showSuggestions && (
+          <div className="absolute top-12 left-0 w-full bg-white shadow-lg border border-gray-200 rounded-lg max-h-64 overflow-y-auto z-10">
+            <ul>
+              { suggestions?.map((suggestion, index) => (
+                <li
+                  key={index}
+                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer flex text-center"
+                >
+                  <FaSearch  className="text-center m-2"/> {suggestion}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
 
       {/* Right Section - User Icon */}
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,8 @@
 const GOGGLE_API_KEY = "AIzaSyCDh3jVoCBLKSa_uJrELVGp2KIRvB8Bgjs";
 
 export const YOUTUBE_VIDEO_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOGGLE_API_KEY;
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOGGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
