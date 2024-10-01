a834614c98e2bda7ed595a8cc1038038375b1582
ParmS-Musale <musaleparm9541@gmail.com>
2024-10-01 13:01:10 +0530

    Day 2 Completed..!!

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from "react";
 import { SlMenu } from "react-icons/sl";
 import { FaUserCircle } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 import { YOUTUBE_SEARCH_API } from "../utils/constant";
+import { cacheResults } from "../utils/searchSlice";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const [showSuggestions, setShowSuggestions] = useState(false);
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const searchCache = useSelector((store) => store.search);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (searchQuery) {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
         getSearchSuggestions();
       }
     }, 200);
+
     return () => {
       clearTimeout(timer);
     };
@@ -26,13 +33,19 @@ const Header = () => {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
     setSuggestions(json[1]);
-  };
 
-  const dispatch = useDispatch();
+    // Dispatch the cache action here where `json` is defined
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
+
 
   return (
     <div className="flex items-center justify-between p-4 bg-white shadow-md">
@@ -67,12 +80,12 @@ const Header = () => {
         {showSuggestions && (
           <div className="absolute top-12 left-0 w-full bg-white shadow-lg border border-gray-200 rounded-lg max-h-64 overflow-y-auto z-10">
             <ul>
-              { suggestions?.map((suggestion, index) => (
+              {suggestions?.map((suggestion, index) => (
                 <li
                   key={index}
                   className="px-4 py-2 hover:bg-gray-100 cursor-pointer flex text-center"
                 >
-                  <FaSearch  className="text-center m-2"/> {suggestion}
+                  <FaSearch className="text-center m-2" /> {suggestion}
                 </li>
               ))}
             </ul>
